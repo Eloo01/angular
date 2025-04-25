d4ad3d6ee8b65c46f8b0daaded8b64d3e4969070
makharamovi <maxaramovielimxan@gmail.com>
2025-04-25 05:08:56 +0400

    added burger menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
+import { CommonModule } from '@angular/common';  
 import { RouterModule } from '@angular/router';
-
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule],
+  standalone: true,
+  imports: [CommonModule, RouterModule],  
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
 
+  isMenuOpen: boolean = false;
+
+  constructor(private renderer: Renderer2) {}
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+
+    // Scroll engelleme
+    document.body.style.overflow = this.isMenuOpen ? 'hidden' : 'auto';
+
+    // body'ye class ekleyip çıkart (isteğe bağlı)
+    if (this.isMenuOpen) {
+      this.renderer.addClass(document.body, 'menu-open');
+    } else {
+      this.renderer.removeClass(document.body, 'menu-open');
+    }
+  }
+  
+  closeMenu() {
+    this.isMenuOpen = false;
+    document.body.style.overflow = 'auto';
+    this.renderer.removeClass(document.body, 'menu-open');
+  }
 }
+
+
+
+
 
 
